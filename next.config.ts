a22e661b607d566c977959cf6ba3d6891ebc744e
SIKU-KR <peter012677@naver.com>
2025-05-07 16:42:09 +0900

    refactor: 디렉터리 구조개선

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     // 빌드 시 ESLint 검사를 비활성화합니다 (프로덕션에서만 사용하세요)
     ignoreDuringBuilds: true,
   },
+  postcssConfig: './config/postcss.config.mjs',
 };
 
 export default nextConfig;
