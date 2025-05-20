49cc23563ba260a391bb3c6c92276e2ccec2a6bf
Bumshik Park <peter012677@naver.com>
2025-05-20 17:18:21 +0900

    feat: Google Analytics 스크립트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import ClientLayout from './client-layout';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Siku 기술블로그',
@@ -14,6 +15,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" data-color-mode="light">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z527NWJYX3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z527NWJYX3');
+          `}
+        </Script>
+      </head>
       <body>
         <ClientLayout>{children}</ClientLayout>
       </body>
